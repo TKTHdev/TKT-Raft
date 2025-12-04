e0bdd6144ca2d4d97729769f1afff7a04768ff37
TKTHdev <tkt0430@keio.jp>
2025-12-05 02:08:25 +0900

    constructor

--- raft.go
+++ raft.go
@@ -28,6 +28,24 @@ type Raft struct {
 	state       int
 }
 
+func NewRaft(id string, peers []string) *Raft {
+	r := &Raft{
+		conns:       make(map[string]*rpc.Client),
+		currentTerm: 0,
+		votedFor:    -1,
+		log:         make([]LogEntry, 0),
+		commitIndex: 0,
+		lastApplied: 0,
+		nextIndex:   make(map[int]int),
+		matchIndex:  make(map[int]int),
+		id:          id,
+		state:       FOLLOWER,
+	}
+	go r.listenRPC()
+	go r.initConns(peers)
+	return r
+}
+
 func main() {
 
 }
