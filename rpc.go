9d52261b5d7d73188e6f0a726c4dbc998d74e607
TKTHdev <tkt0430@keio.jp>
2025-12-05 23:13:57 +0900

    AppendEntries

--- rpc.go
+++ rpc.go
@@ -32,12 +32,47 @@ type RequestVoteReply struct {
 }
 
 func (r *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply) error {
+	//1. Reply false if term < currentTerm
 	if args.Term < r.currentTerm {
 		reply.Term = r.currentTerm
 		reply.Success = false
 		return nil
 	}
-
+	//2. Reply false if log doesn't contain an entry at prevLogIndex whose term matches prevLogTerm
+	if args.PrevLogIndex >= len(r.log) || r.log[args.PrevLogIndex].Term != args.PrevLogTerm {
+		reply.Term = r.currentTerm
+		reply.Success = false
+		return nil
+	}
+	//3. If an existing entry conflicts with a new one (same index but different terms), delete the existing entry and all that follow it
+	for i, entry := range args.Entries {
+		logIndex := args.PrevLogIndex + 1 + i
+		if logIndex < len(r.log) {
+			if r.log[logIndex].Term != entry.Term {
+				r.log = r.log[:logIndex]
+				break
+			}
+		}
+	}
+	//4. Append any new entries not already in the log
+	for i, entry := range args.Entries {
+		logIndex := args.PrevLogIndex + 1 + i
+		if logIndex >= len(r.log) {
+			r.log = append(r.log, entry)
+		}
+	}
+	//5. If leaderCommit > commitIndex, set commitIndex = min(leaderCommit, index of last new entry)
+	if args.LeaderCommit > r.commitIndex {
+		lastNewEntryIndex := args.PrevLogIndex + len(args.Entries)
+		if args.LeaderCommit < lastNewEntryIndex {
+			r.commitIndex = args.LeaderCommit
+		} else {
+			r.commitIndex = lastNewEntryIndex
+		}
+	}
+	reply.Term = r.currentTerm
+	reply.Success = true
+	return nil
 }
 
 func (r *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) error {
